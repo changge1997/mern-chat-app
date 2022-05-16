2199013a4f8ef6bdd8acb682b0d5ce9c8974e870
changge1997 <98774402+changge1997@users.noreply.github.com>
2022-05-15 23:40:43 -0400

    make it better

--- backend/server.js
+++ backend/server.js
@@ -25,14 +25,13 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    const index = path.join(__dirname, "frontend", "build", "index.html");
+    res.sendFile(index);
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running successfully...");
