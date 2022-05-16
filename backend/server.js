5f610fc62eb7bc42df1ae27e0f95e3917a68cc12
changge1997 <98774402+changge1997@users.noreply.github.com>
2022-05-16 00:16:19 -0400

    make it better

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
-
+console.log(path.resolve());
 dotenv.config();
 connectDB();
 const app = express();
@@ -26,10 +26,10 @@ app.use("/api/message", messageRoutes);
 // --------------------------deployment------------------------------
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(path.resolve(), "/frontend/build")));
 
   app.get("*", (req, res) => {
-    const index = path.join(__dirname, "frontend", "build", "index.html");
+    const index = path.join(path.resolve(), "frontend", "build", "index.html");
     res.sendFile(index);
   });
 } else {
