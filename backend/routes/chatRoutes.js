de7211d7dc56c3e900ec29dadd51716ed9f192f6
changge1997 <98774402+changge1997@users.noreply.github.com>
2022-05-15 22:21:11 -0400

    make it better

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  removeFromGroup,
+  addToGroup,
+  renameGroup,
+} = require("../controllers/chatControllers");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.route("/").post(protect, accessChat);
+router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, createGroupChat);
+router.route("/group-rename").put(protect, renameGroup);
+router.route("/group-remove").put(protect, removeFromGroup);
+router.route("/group-add").put(protect, addToGroup);
+
+module.exports = router;
